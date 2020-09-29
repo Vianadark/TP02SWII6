e857dc49749db8a5d90c481572238f72e9a067ba
ericlesn <ericlesnunes1@outlook.com>
2020-09-29 20:20:03 -0300

    feito

---
+++ PortoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TP02SWII6.Models;
+
+namespace TP02SWII6
+{
+    public class PortoContext : DbContext
+    {
+        public DbSet<BL> BLs { get; set; }
+        public DbSet<Container> Containers { get; set; }
+
+        public PortoContext(DbContextOptions<PortoContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
